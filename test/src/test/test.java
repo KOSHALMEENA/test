21cd1d754bd9a61f1718ccb73b1839b35e8841bc
Koshal Meena <Koshal Meena@DESKTOP-AGAT0CO>
2017-02-06 14:35:14 +0530

    HEY

---
+++ test/src/test/test.java
@@ -0,0 +1,11 @@
+package test;
+
+public class test {
+public static void main(String args[])
+{
+	System.out.println("hlw koshal");
+	System.out.println("hlw aman");
+	System.out.println("hlw chetan");
+	System.out.println("hlw newtan");
+}
+}
